d3c562989d2f33e6618f4e8daf755ea20bd8b85c
Potapov Sergey <blake131313@gmail.com>
2012-02-08 18:43:26 +0200

    More specs for Dumper
    Task 2125
    Task 2209

--- spec/active_record/schema_dumper_spec.rb
+++ spec/active_record/schema_dumper_spec.rb
@@ -9,25 +9,45 @@ describe ActiveRecord::SchemaDumper do
       @dump = stream.string
     end
 
-    it 'creates schemas' do
-      @dump.should =~ /create_schema "demography"/
+    context 'Schemas' do
+      it 'dumps schemas' do
+        @dump.should =~ /create_schema "demography"/
+      end
+
+      it 'dumps tables from non public schemas' do
+        @dump.should =~ /create_table "users"/
+        @dump.should =~ /create_table "demography.citizens"/
+        @dump.should =~ /create_table "demography.countries"/ 
+      end
+
+      it 'dumps indexes' do
+        @dump.should =~ /add_index "users", \["name"\]/
+        @dump.should =~ /add_index "demography\.citizens", \["country_id"\]/
+        @dump.should =~ /add_index "demography\.citizens", \["user_id"\]/
+      end
     end
 
-    it 'creates tables' do
-      @dump.should =~ /create_table "users"/
-      @dump.should =~ /create_table "demography.citizens"/
-      @dump.should =~ /create_table "demography.countries"/ 
+    context 'Foreign keys' do
+      it 'dumps from public schema' do
+        @dump.should =~ /^\s*add_foreign_key "pets", "users", :name => "pets_user_id_fk"$/
+      end
+
+      it 'dumps from non public schemas' do
+        @dump.should =~ /^\s*add_foreign_key "demography\.citizens", "users", :name => "demography_citizens_user_id_fk"$/
+      end
     end
 
-    it 'creates indexes' do
-      @dump.should =~ /add_index "users", \["name"\]/
-      @dump.should =~ /add_index "demography\.citizens", \["country_id"\]/
-      @dump.should =~ /add_index "demography\.citizens", \["user_id"\]/
+    context 'Comments' do
+      it 'dumps table comments' do
+        @dump.should =~ /set_table_comment 'users', 'Information about users'/
+        @dump.should =~ /set_table_comment 'demography.citizens', 'Citizens Info'/
+      end
+
+      it 'dumps column comments' do
+        @dump.should =~ /set_column_comment 'users', 'name', 'User name'/
+        @dump.should =~ /set_column_comment 'demography.citizens', 'first_name', 'First name'/
+      end
     end
 
-    it 'foreign keys' do
-      @dump.should =~ /^\s*add_foreign_key "demography\.citizens", "users", :name => "demography_citizens_user_id_fk"$/
-      @dump.should =~ /^\s*add_foreign_key "pets", "users", :name => "pets_user_id_fk"$/
-    end
   end
 end
