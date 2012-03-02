d8c075114ce254d4d835ce2c7870221db8680e55
Matt Dressel <matt.dressel@gmail.com>
2012-03-02 00:55:04 -0600

    Fixed schema dumper indentation for add_foreign_key
    Task 2395 - create_unique_constraint_for_active_rule_sets@pg_power

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -74,10 +74,10 @@ ActiveRecord::Schema.define(:version => 20120224204546) do
 
   set_column_comment 'demography.countries', 'name', 'Country name'
 
- add_foreign_key "demography.cities", "demography.countries", :name => "demography_cities_country_id_fk", :column => "country_id", :exclude_index => true
+  add_foreign_key "demography.cities", "demography.countries", :name => "demography_cities_country_id_fk", :column => "country_id", :exclude_index => true
 
- add_foreign_key "demography.citizens", "public.users", :name => "demography_citizens_user_id_fk", :column => "user_id", :exclude_index => true
+  add_foreign_key "demography.citizens", "public.users", :name => "demography_citizens_user_id_fk", :column => "user_id", :exclude_index => true
 
- add_foreign_key "pets", "public.users", :name => "pets_user_id_fk", :column => "user_id", :exclude_index => true
+  add_foreign_key "pets", "public.users", :name => "pets_user_id_fk", :column => "user_id", :exclude_index => true
 
 end
