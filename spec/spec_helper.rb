362c14b5dea62d2de489f9614e9939fb6fdc2641
Arthur Shagall <arthur.shagall@gmail.com>
2013-02-23 12:33:42 -0600

    Adding simplecov test coverage measurements.
    This enables simplecov C0 test coverage metrics if running in Ruby 1.9.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,12 @@
 ENV["RAILS_ENV"] ||= 'test'
+# We probably don't want to use simplecov in Travis:CI, and we want to confirm
+# simplecov exists (it won't in ruby 1.8.X)
+if !ENV['TRAVIS'] && require('simplecov')
+  SimpleCov.start do
+    add_filter 'spec'
+  end
+end
+
 require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
