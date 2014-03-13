ebcc6820a1b8790b36151115f72cc4eed1b2a63c
Yoshinori Kawasaki <ykawasaki@gmail.com>
2014-03-13 19:22:59 +0900

    Duplicate options param in methods with side-effect to the options param

--- lib/pg_power/connection_adapters/abstract_adapter/schema_methods.rb
+++ lib/pg_power/connection_adapters/abstract_adapter/schema_methods.rb
@@ -4,6 +4,7 @@ module PgPower::ConnectionAdapters::AbstractAdapter::SchemaMethods
 
   # Provide :schema option to +create_table+ method.
   def create_table_with_schema_option(table_name, options = {}, &block)
+    options = options.dup
     schema_name = options.delete(:schema)
     table_name  = "#{schema_name}.#{table_name}" if schema_name
     create_table_without_schema_option(table_name, options, &block)
@@ -11,6 +12,7 @@ module PgPower::ConnectionAdapters::AbstractAdapter::SchemaMethods
 
   # Provide :schema option to +drop_table+ method.
   def drop_table_with_schema_option(table_name, options = {})
+    options = options.dup
     schema_name = options.delete(:schema)
     table_name  = "#{schema_name}.#{table_name}" if schema_name
     drop_table_without_schema_option(table_name, options)
