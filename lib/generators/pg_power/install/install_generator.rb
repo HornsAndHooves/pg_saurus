b71fbc5d03364604e93ae27b698ea1ef5cc64cb9
Potapov Sergey <blake131313@gmail.com>
2014-09-11 17:11:40 +0300

    Add missing doc
    Task 1004

--- lib/generators/pg_power/install/install_generator.rb
+++ lib/generators/pg_power/install/install_generator.rb
@@ -1,6 +1,10 @@
 module PgPower
+  # :nodoc:
   module Generators
+    # Generates config/initializers/pg_power.rb with default settings.
     class InstallGenerator < ::Rails::Generators::Base
+
+      # :nodoc:
       desc <<-DESC
 Description:
   Create default PgPower configuration
@@ -8,9 +12,11 @@ DESC
 
       source_root File.expand_path('../templates', __FILE__)
 
+      # :nodoc:
       def copy_rails_files
         template "config/initializers/pg_power.rb"
       end
+
     end
   end
 end
