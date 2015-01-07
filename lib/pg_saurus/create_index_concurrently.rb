8864cf1407f61ba92346923bfe51c6ca356316f7
Mark Friedgan <hubrix+github@gmail.com>
2015-01-07 15:41:44 -0600

    need to just remove the proper_name thing alltogether

--- lib/pg_saurus/create_index_concurrently.rb
+++ lib/pg_saurus/create_index_concurrently.rb
@@ -70,7 +70,6 @@ module PgSaurus::CreateIndexConcurrently
     #
     # @see ActiveRecord::ConnectionAdapters::SchemaStatements.add_index in pg_saurus gem
     def add_index(table_name, column_name, options = {}, &block)
-      table_name = to_table
       # GOTCHA:
       #   checks if index should be created concurretnly then put it into
       #   the queue to wait till queue processing will be called (should be
@@ -108,7 +107,6 @@ module PgSaurus::CreateIndexConcurrently
     #
     # @see ::PgSaurus::ConnectionAdapters::PostgreSQLAdapter::ForeignerMethods.add_foreign_key
     def add_foreign_key(from_table, to_table, options = {}, &block)
-      from_table       = ::ActiveRecord::Migrator.proper_table_name(from_table)
       concurrent_index = options[:concurrent_index]
 
       if concurrent_index then
