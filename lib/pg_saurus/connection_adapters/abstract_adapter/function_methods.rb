d1b2105f54a66324541cba94c73f58d9e55a3cd5
Arthur Shagall <arthur.shagall@gmail.com>
2015-07-15 03:15:24 -0500

    Updated documentation.

--- lib/pg_saurus/connection_adapters/abstract_adapter/function_methods.rb
+++ lib/pg_saurus/connection_adapters/abstract_adapter/function_methods.rb
@@ -7,11 +7,33 @@ module PgSaurus::ConnectionAdapters::AbstractAdapter::FunctionMethods
   end
 
   # Create a database function.
+  #
+  # Example:
+  #
+  #   # Arguments are: function_name, return_type, function_definition, options (currently, only :schema)
+  #   create_function 'pets_not_empty()', :boolean, <<-FUNCTION, schema: 'public'
+  #     BEGIN
+  #       IF (SELECT COUNT(*) FROM pets) > 0
+  #       THEN
+  #       RETURN true;
+  #       ELSE
+  #       RETURN false;
+  #      END IF;
+  #       END;
+  #     FUNCTION
+  #
+  # The schema is optional.
   def create_function(function_name, returning, definition, options = {})
 
   end
 
   # Delete the database function.
+  #
+  # Example:
+  #
+  #   drop_function 'pets_not_empty()', schema: 'public'
+  #
+  # The schema is optional.
   def drop_function(function_name, options)
 
   end
