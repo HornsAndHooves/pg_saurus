0f77e525cdb7ac18aa502acf2f43c4f0e827b7f7
Arthur Shagall <arthur.shagall@gmail.com>
2016-05-03 19:34:30 -0500

    Dead code cleanup.

--- lib/pg_saurus/connection_adapters/abstract_adapter.rb
+++ lib/pg_saurus/connection_adapters/abstract_adapter.rb
@@ -4,14 +4,12 @@ module PgSaurus::ConnectionAdapters::AbstractAdapter
   extend ActiveSupport::Concern
 
   autoload :CommentMethods
-  # autoload :ForeignerMethods
   autoload :SchemaMethods
   autoload :IndexMethods
   autoload :FunctionMethods
   autoload :TriggerMethods
 
   include CommentMethods
-  # include ForeignerMethods
   include SchemaMethods
   include IndexMethods
   include FunctionMethods
@@ -19,6 +17,6 @@ module PgSaurus::ConnectionAdapters::AbstractAdapter
 
   included do
     alias_method_chain :create_table, :schema_option
-    #alias_method_chain :drop_table  , :schema_option
+    alias_method_chain :drop_table  , :schema_option
   end
 end
