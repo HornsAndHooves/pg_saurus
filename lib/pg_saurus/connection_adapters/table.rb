cdf397da476a1207951dc045cd6581b4bea2c497
Arthur Shagall <arthur.shagall@gmail.com>
2016-05-03 19:52:55 -0500

    Remove obsolete code.

--- lib/pg_saurus/connection_adapters/table.rb
+++ lib/pg_saurus/connection_adapters/table.rb
@@ -2,17 +2,11 @@
 # to support pg_saurus features.
 module PgSaurus::ConnectionAdapters::Table
   extend ActiveSupport::Autoload
-  extend ActiveSupport::Concern
 
   autoload :CommentMethods
-  autoload :ForeignKeyMethods
   autoload :TriggerMethods
 
   include CommentMethods
-  include ForeignKeyMethods
   include TriggerMethods
 
-  included do
-    alias_method_chain :references, :foreign_keys
-  end
 end
